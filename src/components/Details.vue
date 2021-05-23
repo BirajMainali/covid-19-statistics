27938058716d81c907caef648d1d3c31d786c726
Biraj Mainali <biraj.prathamit@gmail.com>
2021-05-23 21:14:30 +0545

    feat :Home Active work

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,89 +1,131 @@
 <script>
+// @ts-nocheck
+
 import axios from "axios";
+
+function GetInitialCountryInfo() {
+  return {
+    Population: "0",
+    New: "0",
+    Active: "0",
+    Critical: "0",
+    Recovered: "0",
+    CasesPM: "0",
+    TotalCases: "0",
+    NewDeath: "0",
+    DeathPM: "0",
+    TotalDeath: "0",
+    TestPM: "0",
+    Totaltest: "0",
+    Day: "0",
+  };
+}
 
 export default {
   name: "Details",
   data() {
     return {
+      SelectedCountry: "NEPAL",
       countries: [],
-      CountryInfo: {
-        ActiveCases: '',
-        CountryName: '',
-        NewCases: '',
-        NewDeath: '',
-        TotalCase: '',
-        TotalDeath: '',
-
-      },
-    }
+      CountryInfo: GetInitialCountryInfo(),
+    };
   },
   methods: {
-    async getCountryInfo() {
+    async getCountriesStatistics() {
       try {
-        const options = {
-          method: 'GET',
-          url: 'https://covid-19-data.p.rapidapi.com/report/country/name',
-          params: {name: 'Italy', date: '2020-04-01'},
-          headers: {
-            'x-rapidapi-key': '1b2899e21dmsh530f29776d8ce27p121576jsne067b81e370e',
-            'x-rapidapi-host': 'covid-19-data.p.rapidapi.com'
-          }
-        };
-        return await axios.request(options);
+        return await axios(
+            `https://covid-193.p.rapidapi.com/statistics?country=${this.SelectedCountry}`,
+            {
+              method: "GET",
+              responseType: 'json',
+              headers: {
+                "x-rapidapi-key":
+                    "1b2899e21dmsh530f29776d8ce27p121576jsne067b81e370e",
+                "x-rapidapi-host": "covid-193.p.rapidapi.com",
+              },
+            }
+        );
       } catch (e) {
-        console.log(e);
+        console.warn(e);
       }
     },
-    renderCountryInfo(county) {
-      this.CountryInfo.push({...[county]});
+    renderCountriesStatistics(Statistics) {
+      this.CountryInfo.Population = Statistics.population;
+      this.CountryInfo.New = Statistics.cases.new;
+      this.CountryInfo.Active = Statistics.cases.active;
+      this.CountryInfo.Critical = Statistics.cases.critical;
+      this.CountryInfo.Recovered = Statistics.cases.recovered;
+      this.CountryInfo.CasesPM = Statistics.cases["1M_pop"];
+      this.CountryInfo.TotalCases = Statistics.cases.total;
+      /*Cases*/
+      this.CountryInfo.NewDeath = Statistics.deaths.new;
+      this.CountryInfo.DeathPM = Statistics.deaths["1M_pop"];
+      this.CountryInfo.TotalDeath = Statistics.deaths.total;
+      /*Death*/
+      this.CountryInfo.TestPM = Statistics.tests["1M_pop"]
+      this.CountryInfo.Totaltest = Statistics.tests.total;
+      /*Test*/
+      this.CountryInfo.Day = Statistics.tests.day;
     },
-
-    async LoadCountryInfo() {
-      const res = await this.getCountryInfo();
-      this.renderCountryInfo(res.data);
+    async loadCountriesStatistics() {
+      const res = await this.getCountriesStatistics();
+      return this.renderCountriesStatistics(res.data.response[0]);
     },
 
     async getCountries() {
-      return await axios.get("https://api.printful.com/countries");
+      try {
+        return await axios.get("https://restcountries.eu/rest/v2/all");
+      } catch (e) {
+        console.warn(e);
+      }
     },
     renderCountries(countries) {
       for (let country in countries) {
-        this.countries.push({...countries[country]})
+        this.countries.push({...countries[country]});
       }
     },
-
-    async LoadCountries() {
-      const res = await this.getCountries();
-      this.renderCountries(res.data);
-
+    async loadCountries() {
+      const response = await this.getCountries();
+      return this.renderCountries(response.data);
     },
   },
-  mounted() {
-    this.LoadCountries();
-    this.LoadCountryInfo();
-  }
-}
+  watch: {
+    async SelectedCountry() {
+      if (!this.SelectedCountry) {
+        this.CountryInfo = GetInitialCountryInfo();
+      } else {
+        await this.loadCountriesStatistics();
+      }
+    }
+  },
+  async mounted() {
+    await this.loadCountries();
+    await this.loadCountriesStatistics();
+  },
+};
 </script>
 <template>
   <div class="col-10">
     <div class="card">
       <div class="card-header bg-white">
         <div class="card-title">
-          <h4>Information of Nepal</h4><br>
-          <p>From JHU CSSE COVID-19 Data · Last updated: 2 days ago</p>
+          <h4>Individual Country Details</h4>
+          <br/>
+          <p>Last updated: {{ CountryInfo.Day }}</p>
         </div>
       </div>
       <div class="card-body">
         <div class="row mb-4">
           <div class="col-2">
-            <select class="form-select">
-              <option selected disabled>Total</option>
-            </select>
+            <selected class="form-select">
+              Details
+            </selected>
           </div>
           <div class="col-2">
-            <select class="form-select">
-              <option value="">Nepal</option>
-              <option value="">India</option>
+            <select class="form-select" v-model="SelectedCountry">
+              <option v-for="country in countries" :key="country.name" :value="country.name">
+                {{ country.name }}
+              </option>
             </select>
           </div>
         </div>
@@ -93,21 +135,16 @@ export default {
               <span>Cases</span>
             </div>
             <div class="row">
-              <h5>497K</h5>
+              <h5>{{ CountryInfo.TotalCases }}</h5>
             </div>
-            <div class="row">
-              <p>+8,407</p>
-            </div>
+            <div class="row"></div>
           </div>
           <div class="col-3">
             <div class="row">
-              <span>Recovered</span>
+              <span>Total Test</span>
             </div>
             <div class="row">
-              <h5>497K</h5>
-            </div>
-            <div class="row">
-              <p>+8,407</p>
+              <h5>{{ CountryInfo.Totaltest }}</h5>
             </div>
           </div>
           <div class="col-3">
@@ -115,51 +152,85 @@ export default {
               <span>Deaths</span>
             </div>
             <div class="row">
-              <h5>497K</h5>
+              <h5>{{ CountryInfo.TotalDeath }}</h5>
             </div>
-            <div class="row">
-              <p>+8,407</p>
-            </div>
+            <div class="row"></div>
           </div>
         </div>
       </div>
       <div class="card-footer">
-        <table class="table table-hover">
-          <thead>
-          <tr>
-            <td>Location</td>
-            <td>Cases</td>
-            <td>Recovered</td>
-            <td>Deaths</td>
-          </tr>
-          </thead>
-          <tbody>
-          <tr>
-            <td>Karnali Province</td>
-            <td>7,359</td>
-            <td>-</td>
-            <td>27</td>
-          </tr>
-          <tr>
-            <td>Karnali Province</td>
-            <td>7,359</td>
-            <td>-</td>
-            <td>27</td>
-          </tr>
-          <tr>
-            <td>Karnali Province</td>
-            <td>7,359</td>
-            <td>-</td>
-            <td>27</td>
-          </tr>
-          </tbody>
-        </table>
+        <div class="row">
+          <div class="col-4">
+            <div class="card">
+              <div class="card-header">
+                <h6 class="card-title mb-0" text-info>Active</h6>
+              </div>
+              <div class="card-body">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">
+                    New <span>{{ CountryInfo.New }}</span>
+                  </li>
+                  <li class="list-group-item">
+                    Active <span>{{ CountryInfo.Active }}</span>
+                  </li>
+                  <li class="list-group-item">
+                    Recovered <span>{{ CountryInfo.Recovered }}</span>
+                  </li>
+                  <li class="list-group-item">
+                    1M Pop <span>{{ CountryInfo.DeathPM }}</span>
+                  </li>
+                  <li class="list-group-item">
+                    Total <span>{{ CountryInfo.TotalCases }}</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card">
+              <div class="card-header">
+                <h6 class="card-title mb-0" text-info>Test</h6>
+              </div>
+              <div class="card-body">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">
+                    1M Populaion <span>{{ CountryInfo.TestPM }}</span>
+                  </li>
+                  <li class="list-group-item">
+                    Total {{ CountryInfo.Totaltest }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card">
+              <div class="card-header">
+                <h6 class="card-title mb-0" text-info>Death</h6>
+              </div>
+              <div class="card-body">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">
+                    New <span>{{ CountryInfo.NewDeath }}</span>
+                  </li>
+                  <li class="list-group-item">
+                    1M Pop {{ CountryInfo.DeathPM }}
+                  </li>
+                  <li class="list-group-item">
+                    Total {{ CountryInfo.TotalDeath }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-
 <style scoped>
-
+.card {
+  margin-right: 8px;
+}
 </style>
--- src/components/SideBarSummary.vue
+++ src/components/SideBarSummary.vue
@@ -1,42 +1,85 @@
+<script>
+import axios from "axios";
+
+export default {
+  name: "SideBarSummary",
+  data() {
+    return {
+      DailyReportTotals: {
+        NewConfirmed: '',
+        TotalConfirmed: '',
+        NewDeaths: '',
+        TotalDeaths: '',
+        NewRecovered: '',
+        TotalRecovered: '',
+      }
+    }
+  },
+  methods: {
+    async getDailyReportTotals() {
+      try {
+        return await axios(`https://api.covid19api.com/summary`);
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    renderDailyReportTotals(data) {
+      this.DailyReportTotals.NewConfirmed = data.NewConfirmed;
+      this.DailyReportTotals.TotalConfirmed = data.TotalConfirmed;
+      this.DailyReportTotals.NewDeaths = data.NewDeaths;
+      this.DailyReportTotals.TotalDeaths = data.TotalDeaths;
+      this.DailyReportTotals.NewRecovered = data.NewRecovered;
+      this.DailyReportTotals.TotalRecovered = data.TotalRecovered
+    },
+    async loadDailyReportTotals() {
+      const res = await this.getDailyReportTotals()
+      console.log(res.Global);
+      return this.renderDailyReportTotals(res.data.Global)
+    },
+  },
+  async mounted() {
+    await this.loadDailyReportTotals();
+  }
+}
+</script>
 <template>
   <div class="col-2">
     <div class="card">
-      <div class="card-body">
-        <select class="form-select my-select">
-          <option selected disabled>Total</option>
-        </select>
-        <select class="form-select mt-1">
-          <option selected disabled>World Wide</option>
-        </select>
+      <div class="card-header side-header">
+        <h5 class="card-title ">WorldWide Summary</h5>
       </div>
-      <div class="card-footer text-center">
-        <div class="row">
-          <h6>cases</h6>
-          <h2>323232</h2>
-        </div>
-        <div class="row">
-          <h6>Recovered</h6>
-          <h2>323232</h2>
-        </div>
-        <div class="row">
-          <h6>Deaths</h6>
-          <h2>323232</h2>
-        </div>
-        <div class="row">
-          <h6>Other</h6>
-          <h2>323232</h2>
+      <div class="card-body">
+        <div class="card-footer text-center border-none">
+          <ul class="list-group list-group-flush text-start side-summary">
+            <li class="list-group-item mt-3">NewConfirmed<h4 class="text-end">{{ DailyReportTotals.NewConfirmed }}</h4>
+            </li>
+            <li class="list-group-item mt-3">TotalConfirmed<h4 class="text-end">{{
+                DailyReportTotals.TotalConfirmed
+              }}</h4></li>
+            <li class="list-group-item mt-3">NewDeaths<h4 class="text-end">{{ DailyReportTotals.NewDeaths }}</h4></li>
+            <li class="list-group-item mt-3">TotalDeaths<h4 class="text-end">{{ DailyReportTotals.TotalDeaths }}</h4>
+            </li>
+            <li class="list-group-item mt-3">NewRecovered<h4 class="text-end">{{ DailyReportTotals.NewRecovered }}</h4>
+            </li>
+            <li class="list-group-item mt-3">TotalRecovered<h4 class="text-end">{{
+                DailyReportTotals.TotalRecovered
+              }}</h4></li>
+          </ul>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "SideBarSummary"
-}
-</script>
-
 <style scoped>
+.card {
+  padding: 20px;
+  border-radius: 20px;
+  margin-left: 5px;
+}
+
+.card-header {
+  background-color: white;
+}
 
 </style>

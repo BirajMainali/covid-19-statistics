792dd86b54e07a1ce28ec79811d1e2f9eef28e1d
Biraj Mainali <biraj.prathamit@gmail.com>
2021-05-27 20:40:36 +0545

    feat : stuff

--- src/components/TopNavigation.vue
+++ src/components/TopNavigation.vue
@@ -2,7 +2,8 @@
   <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container-fluid ">
       <router-link class="navbar-brand text-white" to="/">Covid-19</router-link>
-      <button aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"
+      <button aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"
+              class="navbar-toggler"
               data-bs-target="#navbarNavAltMarkup" data-bs-toggle="collapse" type="button">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -12,6 +13,9 @@
           <router-link class="nav-link text-white" to="/Data">Data</router-link>
         </div>
       </div>
+      <a href="https://github.com/birajmainali" class="nav-link" style="float: right; color: black">
+        <i class="fab fa-github"></i>
+      </a>
     </div>
   </nav>
 </template>
